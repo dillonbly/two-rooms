aeea620e3a0d296d0fce38ff2fa3de7f6005ae10
Dillon Bly <dillonbly@gmail.com>
2014-05-11 21:30:17 -0700

    Remove dead views.

--- app/js/app.js
+++ app/js/app.js
@@ -10,10 +10,8 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.when('/menu', {templateUrl: 'partials/menu.html', controller: 'MenuCtrl'});
   $routeProvider.when('/settings', {templateUrl: 'partials/settings.html', controller: 'SettingsCtrl'});
   $routeProvider.when('/timer', {templateUrl: 'partials/timer.html', controller: 'TimerCtrl'});
-  $routeProvider.otherwise({redirectTo: '/menu'});
+  $routeProvider.otherwise({redirectTo: '/timer'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -82,9 +82,3 @@ angular.module('myApp.controllers', [])
   .controller('MenuCtrl', ['$scope', function($scope) {
 
   }])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
-  }]);
